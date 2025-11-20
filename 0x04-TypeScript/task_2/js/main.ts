c9f80f78cb152c4adf7b4b7cf60a18d5dd03201b
Billyrono <billyrono46@gmail.com>
2025-11-20 19:50:20 +0300

    Add Task_2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -22,21 +22,21 @@ function createEmployee(salary: number | string): Director | Teacher {
     // @ts-ignore
   if (salary < 500) {
     return new Teacher();
-  } 
+  }   
   return new Director();
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
+export function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
-function executeWork(employee: Director | Teacher): void {
+export function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
-    console.log(employee.workDirectorTasks());
+    return employee.workDirectorTasks();
   } else {
-    console.log(employee.workTeacherTasks());
+    return employee.workTeacherTasks();
   }
 }
 
-executeWork(createEmployee(200));
-executeWork(createEmployee(1000));
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000))); 
