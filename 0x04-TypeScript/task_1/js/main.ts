0a34d6bcac2ef4afb918ec8297977f4188584290
Billyrono <billyrono46@gmail.com>
2025-11-20 18:23:34 +0300

    Add Task_2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,43 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+interface DirectorInterface {
+  workDirectorTasks(): string;
 }
 
-const teacher3: Teacher = {
-  firstName: 'Francis',
-  lastName: 'Gitau',
-  fullTimeEmployee: false,
-  location: 'Kirinyaga',
-  contract: false,
-};
+interface TeacherInterface {
+  workTeacherTasks(): string;
+}
 
-console.log(teacher3);
+class Director implements DirectorInterface {
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+    // @ts-ignore
+  if (salary < 500) {
+    return new Teacher();
+  } 
+  return new Director();
+}
+
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
